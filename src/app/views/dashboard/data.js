6a621a35dda48918f5266eae649980328d5cb68b
Josue Mutabazi <mutajoshua99@gmail.com>
2021-08-21 21:33:52 +0200

    Revising tools and resources

--- src/app/views/dashboard/data.js
+++ src/app/views/dashboard/data.js
@@ -1,135 +1,135 @@
 /* 
-export const ref=[
+export const ref: [
     {
       title:"WHO IHR Monitoring and Evaluation Framework",
       description:"The IHR MEF identifies gaps in capacity and agreed priority actions, many of which require engagement of multiple sectors and/or relate to zoonoses and other health threats at The human-animal-environment interface.  Countries can use The Tripartite zoonoses Guide as appropriate to bring together stakeholders to fill these gaps and undertake these priority actions",
-      href:"https://apps.who.int/iris/bitstream/handle/10665/276651/WHO-WHE-CPI-2018.51-eng.pdf?sequence=1",
-      catId=13,
+      href:"https://apps.who.int/iris/bitstream/handle/10665/276651/WHO-WHE-CPI-2018.51-eng.pdf?sequence: 1",
+      catId: 13,
     },
     {
       title:"Support to national legislation for IHR implementation ",
       description:" Sector-specific tool aimed at harmonizing existing legal frameworks with the IHR framework during implementation at country level  ",
-      href:"https://www.who.int/ihr/Intro_legislative_implementation.pdf?ua=1",
-      catId=1,
+      href:"https://www.who.int/ihr/Intro_legislative_implementation.pdf?ua: 1",
+      catId: 1,
     },
     {
       title:"OIE Performance of Veterinary Services Pathway ",
       description:"The OIE PVS Pathway has a strong systems approach and as such supports the strengthening of multisectoral capacities needed in the control of zoonoses, food safety and AMR.  A country might repeat the process every few years (typically every 5 years) to measure improvement and identify ongoing gaps",
       href:"https://www.oie.int/fileadmin/Home/eng/Support_to_OIE_Members/pdf/A_PVS_Tool_Final_Edition_2013.pdf",
-      catId=13,
+      catId: 13,
     },
     {
       title:"Food and Nutrition Security Impact, Resilience, Sustainability and Transformation [FIRST] Policy Guidance Notes",
       description:"Ensures that policy makers have the support they need to understand how sectoral issues affect food security and nutrition and to recognise how to incorporate these considerations into their policies.This sector-specific tool can be used by countries in building capacity to take a One Health approach ",
       href:"http://www.fao.org/publications/policy-guidance-series/en/",
-      catId=11,
+      catId: 11,
     },
     {
       title:"Blueprint for Canine Rabies Prevention and Control",
       description:"Developed to enable ministries and other interested parties to help themselves through the process of designing, implementing and evaluating a large scale rabies control programme.This multisectoral tool can be used by countries taking a One Health approach ",
       href:"https://rabiesblueprint.org/",
-      catId=231,
+      catId: 231,
     },
     {
       title:"A Tripartite Guide to Addressing Zoonotic Diseases in Countries (TZG)",
       description:"The TZG can be used by countries to fill gaps identified by any number of national assessments of capacity, and to conduct priority activities identified by various processes and details in national plans",
-      href:"https://www.who.int/foodsafety/zoonoses/final_concept_note_Hanoi.pdf?ua=1",
-      catId=11,
+      href:"https://www.who.int/foodsafety/zoonoses/final_concept_note_Hanoi.pdf?ua: 1",
+      catId: 11,
     },
     {
       title:"World Bank  Environmental and Social Services Framework (ESF)",
       description:"This sector-specific framework can be used by countries in building capacity to take a One Health approach",
       href:"http://documents.worldbank.org/curated/en/383011492423734099/pdf/114278-WP-REVISED-PUBLIC-Environmental-and-Social-Framework.pdf",
-      catId=22,
+      catId: 22,
     },
     {
       title:"IHR-PVS National Bridging Workshops ",
       description:" Results of the IHR/PVS NBWs can feed the development of the National Action Plan for Health Security (NAPHS) or other national One Health plans. Countries can use the Tripartite Zoonoses Guide as appropriate to bring together stakeholders to undertake actions agreed upon under this framework",
       href:"https://extranet.who.int/sph/ihr-pvs-bridging-workshop",
-      catId=2,
+      catId: 2,
     },
     {
       title:"Joint External Evaluation ",
       description:"The JEE identifies gaps in capacity and agreed priority actions, many of which require engagement of multiple sectors and/or relate to zoonoses and other health threats at the human-animal-environment interface",
       href:"https://extranet.who.int/sph/sites/default/files/document-library/document/9789241550222-eng.pdf",
-      catId=2,
+      catId: 2,
     },
     {
       title:"States Parties Annual Reporting to the World Health Assembly (SPAR)",
       description:"The SPAR identifies gaps in capacity, many of which require engagement of multiple sectors and/or relate to zoonoses and other health threats at the human-animalenvironment interface",
-      href:"https://apps.who.int/iris/bitstream/handle/10665/272432/WHO-WHE-CPI-2018.16-eng.pdf?sequence=1",
-      catId=0,
+      href:"https://apps.who.int/iris/bitstream/handle/10665/272432/WHO-WHE-CPI-2018.16-eng.pdf?sequence: 1",
+      catId: 0,
     },
     {
       title:"After Action Review",
       description:" AARs identify gaps in capacity and agreed priority actions, many of which require engagement of multiple sectors and/or relate to zoonoses and other health threats at the human-animal-environment interface.",
-      href:"http://www.euro.who.int/__data/assets/pdf_file/0009/393651/who-guid-aar-eng.pdf?ua=1",
-      catId=2,
+      href:"http://www.euro.who.int/__data/assets/pdf_file/0009/393651/who-guid-aar-eng.pdf?ua: 1",
+      catId: 2,
     },
     {
       title:"Simulation Exercises and Manual",
       description:"SimExs identify gaps in capacity, many of which require engagement of multiple sectors and/or relate to zoonoses and other health threats at the human-animalenvironment interface. ",
       href:"https://extranet.who.int/sph/docs/file/3581",
-      catId=2,
+      catId: 2,
     },
     {
       title:"One Health Systems Mapping and Analysis Resource Toolkit (OH-SMART™)",
       description:"OH-SMART is used for to improve government  coordination mechanisms/ platforms and organisations looking to improve One Health collaboration in practice by moving from relationship-based systems to institutionalised approaches that maximise resources and effort and provide mutual benefit ",
       href:"https://simdos.unud.ac.id/uploads/file_pendidikan_1_dir/0f39f315dec9f06c02be39ec35e555b7.pdf",
-      catId=11,
+      catId: 11,
     },
     {
       title:"Surveillance Evaluation Tool (SET) ",
       description:"SET is focused on evaluation of countries' surveillance for animal diseases including zoonoses, however the audience includes the other relevant sectors and ministries involved in zoonotic disease surveillance, including 29 public health and the environment",
       href:"http://www.fao.org/ag/againfo/programmes/en/empres/news_100818.html",
-      catId=2,
+      catId: 2,
     },
     {
       title:"Assessment Tool for Laboratories and Antimicrobial Resistance Surveillance Systems (ATLASS) ",
       description:"ATLASS supports the use of One Health approaches to address AMR, as the audiences include those authorities in the food and agriculture sectors with responsibility for AMR, which are linked through the national AMR surveillance system to the public health system and laboratories.",
       href:"http://www.fao.org/antimicrobial-resistance/resources/tools/atlass/en/",
-      catId=2,
+      catId: 2,
     },
     {
       title:"Laboratory mapping tool (LMT)",
       description:"The LMT supports laboratory capacity improvement and establishment of an effective network of animal health and veterinary public health laboratories. The LMT informs JEE evaluation and can establish a path for progressive building of diagnostic laboratory capacities to detect and characterize infectious agents, including capacity improvement",
       href:"http://www.fao.org/ag/againfo/programmes/en/empres/news_130514.html",
-      catId=2,
+      catId: 2,
     },
     {
       title:"CDC One Health Zoonotic Disease Prioritization (OHZDP) Tool",
       description:"'The OHZDP process uses a multisectoral, One Health approach to prioritise endemic and emerging zoonotic diseases of major public health concern that should be jointly addressed by national human, animal, and environmental health sectors and other relevant partners",
       href:"https://www.cdc.gov/onehealth/global-activities/prioritization.html",
-      catId=231,
+      catId: 231,
     },
     {
       title:"Stepwise Approach to Rabies Elimination (SARE) ",
       description:"The Stepwise Approach towards Rabies Elimination (SARE) is template that countries may use to develop activities and measure progress towards a national programme and strategy for sustainable rabies prevention, control and eventually elimination",
       href:"https://caninerabiesblueprint.org/IMG/pdf/sare_outline_2017_f.pdf",
-      catId=231,
+      catId: 231,
     },
     {
       title:"One Health APP",
       description:"The One Health Assessment for Planning and Performance (OH-APP) is a monitoring framework for multisectoral coordination mechanisms (MCM) to annually self-assess their organizational capacity and performance to inform planning and development assistance.",
       href:"https://www.onehealthapp.org/about",
-      catId=11,
+      catId: 11,
     }
   ]
 
 
  */
 
-  export const ref=[
+  export const ref: [
     {
       title:"WHO IHR Monitoring and Evaluation Framework",
       description:"The IHR MEF identifies gaps in capacity and agreed priority actions, many of which require engagement of multiple sectors and/or relate to zoonoses and other health threats at The human-animal-environment interface.  Countries can use The Tripartite zoonoses Guide as appropriate to bring together stakeholders to fill these gaps and undertake these priority actions",
-      href:"https://apps.who.int/iris/bitstream/handle/10665/276651/WHO-WHE-CPI-2018.51-eng.pdf?sequence=1",
-      catId=13,
+      href:"https://apps.who.int/iris/bitstream/handle/10665/276651/WHO-WHE-CPI-2018.51-eng.pdf?sequence: 1",
+      catId: 13,
     },
     {
       title:"Support to national legislation for IHR implementation ",
       description:" Sector-specific tool aimed at harmonizing existing legal frameworks with the IHR framework during implementation at country level  ",
-      href:"https://www.who.int/ihr/Intro_legislative_implementation.pdf?ua=1",
-      catId=1,
+      href:"https://www.who.int/ihr/Intro_legislative_implementation.pdf?ua: 1",
+      catId: 1,
     }
   ]
--- src/app/views/data.js
+++ src/app/views/data.js
@@ -237,9 +237,61 @@ export const reference=[
 
 export const ref_tools=[
   {
-    title:"The Joint Cross-Sectoral Risk Assessment (JRA) and Risk Management, focusing on the animal-human interface.",
-    description:"The Joint Cross-Sectoral Risk Assessment (JRA) and Risk Management, focusing on the animal-human interface.",
-    href:" http://www.fao.org/3/cb1520en/CB1520EN.pdf ",
+    title:"IHR-PVS National Bridging Workshops ",
+    description:" Results of the IHR/PVS NBWs can feed the development of the National Action Plan for Health Security (NAPHS) or other national One Health plans. Countries can use the Tripartite Zoonoses Guide as appropriate to bring together stakeholders to undertake actions agreed upon under this framework",
+    href:"https://extranet.who.int/sph/ihr-pvs-bridging-workshop",
+    catId: 2,
+    sc: []
+  },
+  {
+    title:"One Health Systems Mapping and Analysis Resource Toolkit (OH-SMART™)",
+    description:"OH-SMART is used for to improve government  coordination mechanisms/ platforms and organisations looking to improve One Health collaboration in practice by moving from relationship-based systems to institutionalised approaches that maximise resources and effort and provide mutual benefit ",
+    href:"https://simdos.unud.ac.id/uploads/file_pendidikan_1_dir/0f39f315dec9f06c02be39ec35e555b7.pdf",
+    catId: 11,
+    sc: []
+  },
+  {
+    title:"Surveillance Evaluation Tool (SET) ",
+    description:"SET is focused on evaluation of countries' surveillance for animal diseases including zoonoses, however the audience includes the other relevant sectors and ministries involved in zoonotic disease surveillance, including 29 public health and the environment",
+    href:"http://www.fao.org/ag/againfo/programmes/en/empres/news_100818.html",
+    catId: 2,
+    sc: []
+  },
+  {
+    title:"Assessment Tool for Laboratories and Antimicrobial Resistance Surveillance Systems (ATLASS) ",
+    description:"ATLASS supports the use of One Health approaches to address AMR, as the audiences include those authorities in the food and agriculture sectors with responsibility for AMR, which are linked through the national AMR surveillance system to the public health system and laboratories.",
+    href:"http://www.fao.org/antimicrobial-resistance/resources/tools/atlass/en/",
+    catId: 2
+  },
+  {
+    title:"Laboratory mapping tool (LMT)",
+    description:"The LMT supports laboratory capacity improvement and establishment of an effective network of animal health and veterinary public health laboratories. The LMT informs JEE evaluation and can establish a path for progressive building of diagnostic laboratory capacities to detect and characterize infectious agents, including capacity improvement",
+    href:"http://www.fao.org/ag/againfo/programmes/en/empres/news_130514.html",
+    catId: 2,
+  },
+  {
+    title:"CDC One Health Zoonotic Disease Prioritization (OHZDP) Tool",
+    description:"'The OHZDP process uses a multisectoral, One Health approach to prioritise endemic and emerging zoonotic diseases of major public health concern that should be jointly addressed by national human, animal, and environmental health sectors and other relevant partners",
+    href:"https://www.cdc.gov/onehealth/global-activities/prioritization.html",
+    catId: 231,
+  },
+  {
+    title:"Stepwise Approach to Rabies Elimination (SARE) ",
+    description:"The Stepwise Approach towards Rabies Elimination (SARE) is template that countries may use to develop activities and measure progress towards a national programme and strategy for sustainable rabies prevention, control and eventually elimination",
+    href:"https://caninerabiesblueprint.org/IMG/pdf/sare_outline_2017_f.pdf",
+    catId: 231,
+  },
+  {
+    title:"One Health APP",
+    description:" The One Health Assessment for Planning and Performance (OH-APP) is a monitoring framework for multisectoral coordination mechanisms (MCM) to annually self-assess their organizational capacity and performance to inform planning and development assistance. ",
+    href:"https://www.onehealthapp.org/about",
+    catId: 253,
+    sc: []
+  },
+  {
+    title:"Qualitative Joint Risk Assessment Tool (JRA)",
+    description:"An Operational Tool of the Tripartite Zoonoses Guide – Taking a Multisectoral, One Health Approach: A Tripartite Guide to Addressing Zoonotic Diseases in Countries. It  describes a national process for conducting joint risk assessment. As part of a functioning national health system, the animal health sector, the human health sector, and other sectors (e.g. wildlife, environment) conduct sector-specific risk assessments of health events routinely and individually.",
+    href:" http://www.fao.org/documents/card/en/c/cb1520en",
     catId:0,
     sc:[]
   }
@@ -255,10 +307,83 @@ export const ref_resources=[
     sc:[]
   },
   {
-    title:"The Joint Cross-Sectoral Risk Assessment (JRA) and Risk Management, focusing on the animal-human interface.",
-    description:"The Joint Cross-Sectoral Risk Assessment (JRA) and Risk Management, focusing on the animal-human interface.",
-    href:" http://www.fao.org/3/cb1520en/CB1520EN.pdf ",
+    title:"Support to national legislation for IHR implementation ",
+    description:" Sector-specific tool aimed at harmonizing existing legal frameworks with the IHR framework during implementation at country level ",
+    href:"https://www.who.int/ihr/Intro_legislative_implementation.pdf?ua=1",
     catId:0,
     sc:[]
-  }
+  },
+  {
+    title:"OIE Performance of Veterinary Services Pathway ",
+    description:"The OIE PVS Pathway has a strong systems approach and as such supports the strengthening of multisectoral capacities needed in the control of zoonoses, food safety and AMR.  A country might repeat the process every few years (typically every 5 years) to measure improvement and identify ongoing gaps",
+    href:"https://www.oie.int/fileadmin/Home/eng/Support_to_OIE_Members/pdf/A_PVS_Tool_Final_Edition_2013.pdf",
+    catId: 13,
+  },
+  {
+    title:"Food and Nutrition Security Impact, Resilience, Sustainability and Transformation [FIRST] Policy Guidance Notes",
+    description:"Ensures that policy makers have the support they need to understand how sectoral issues affect food security and nutrition and to recognise how to incorporate these considerations into their policies.This sector-specific tool can be used by countries in building capacity to take a One Health approach ",
+    href:"http://www.fao.org/publications/policy-guidance-series/en/",
+    catId: 11,
+  },
+  {
+    title:"Blueprint for Canine Rabies Prevention and Control ",
+    description:" Developed to enable ministries and other interested parties to help themselves through the process of designing, implementing and evaluating a large scale rabies control programme.This multisectoral tool can be used by countries taking a One Health approach ",
+    href:"https://rabiesblueprint.org/ ",
+    catId: 203,
+    sc: []
+  },
+  {
+    title:"A Tripartite Guide to Addressing Zoonotic Diseases in Countries (TZG)",
+    description:"The TZG can be used by countries to fill gaps identified by any number of national assessments of capacity, and to conduct priority activities identified by various processes and details in national plans",
+    href:"https://www.who.int/foodsafety/zoonoses/final_concept_note_Hanoi.pdf?ua: 1",
+    catId: 11,
+  },
+  {
+    title:"World Bank  Environmental and Social Services Framework (ESF)",
+    description:"This sector-specific framework can be used by countries in building capacity to take a One Health approach",
+    href:"http://documents.worldbank.org/curated/en/383011492423734099/pdf/114278-WP-REVISED-PUBLIC-Environmental-and-Social-Framework.pdf",
+    catId: 22,
+  },
+  {
+    title:"Joint External Evaluation ",
+    description:"The JEE identifies gaps in capacity and agreed priority actions, many of which require engagement of multiple sectors and/or relate to zoonoses and other health threats at the human-animal-environment interface",
+    href:"https://extranet.who.int/sph/sites/default/files/document-library/document/9789241550222-eng.pdf",
+    catId: 2,
+  },
+  {
+    title:"States Parties Annual Reporting to the World Health Assembly (SPAR)",
+    description:"The SPAR identifies gaps in capacity, many of which require engagement of multiple sectors and/or relate to zoonoses and other health threats at the human-animalenvironment interface",
+    href:"https://apps.who.int/iris/bitstream/handle/10665/272432/WHO-WHE-CPI-2018.16-eng.pdf?sequence: 1",
+    catId: 0,
+  },
+  {
+    title:"After Action Review",
+    description:" AARs identify gaps in capacity and agreed priority actions, many of which require engagement of multiple sectors and/or relate to zoonoses and other health threats at the human-animal-environment interface.",
+    href:"http://www.euro.who.int/__data/assets/pdf_file/0009/393651/who-guid-aar-eng.pdf?ua: 1",
+    catId: 2,
+  },
+  {
+    title:"Simulation Exercises and Manual",
+    description:"SimExs identify gaps in capacity, many of which require engagement of multiple sectors and/or relate to zoonoses and other health threats at the human-animalenvironment interface. ",
+    href:"https://neoh.onehealthglobal.net ",
+    catId: 2,
+  },
+  {
+    title:"Network for Evaluation of One Health (NEOH) ",
+    description:"It aims to enable future quantitative evaluations of One Health activities and to further the evidence base by developing  and applying a science-based evaluation protocol in a community of experts     ",
+    href:"https://extranet.who.int/sph/docs/file/3581",
+    catId: 26,
+  },
+  {
+    title:"Operational framework for strengthening human, animal and environmental public health systems at their interface  ",
+    description:"The near-term purpose of this Operational Framework is to strengthen public health systems to be better prepared to prevent, detect, respond to, and recover from disease pressures at the human-animal-environment interfaces (i.e., health security) ",
+    href:"https://documents1.worldbank.org/curated/en/703711517234402168/pdf/123023-REVISED-PUBLIC-World-Bank-One-Health-Framework-2018.pdf ",
+    catId: 92,
+  },
+  {
+    title:"OIE wildlife health framework ",
+    description:"It describes how the OIE can reinforce One Health strategies through a Wildlife Health Framework. This responds to a global need to better manage risks from emerging diseases at the human-animal- ecosystems interface, whilst protecting wildlife",
+    href:"https://www.oie.int/app/uploads/2021/03/a-wildlifehealth-conceptnote.pdf ",
+    catId: 26,
+  },
 ]
